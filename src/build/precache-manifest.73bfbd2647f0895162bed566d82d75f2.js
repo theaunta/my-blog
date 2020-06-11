2cd1d13c74385b0af4f0629e67f225fcbf475121
theaunta <shivamkrsingh08@gmail.com>
2020-06-11 17:20:27 +0530

    First Commit

---
+++ src/build/precache-manifest.73bfbd2647f0895162bed566d82d75f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8006dd517320d1aea2c2050a3e93b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f5e00adb0b5acacf86",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a1850b1e29f83404fcfa",
+    "url": "/static/js/2.11aa829e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11aa829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f5e00adb0b5acacf86",
+    "url": "/static/js/main.dbd2ed81.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
